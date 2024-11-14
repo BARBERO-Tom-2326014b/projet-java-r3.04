a73e4bd96e12da53c2953a21a055b2d63bb7a653
Foucher Mathias <mathias.foucher@etu.univ-amu.fr>
2024-11-14 15:46:35 +0100

    LES SERVICES MEDICAUX

--- src/personnage/Elfe.java
+++ src/personnage/Elfe.java
@@ -1,5 +1,9 @@
 package personnage;
 
 public class Elfe extends Creature{
+
+	public Elfe(String nomComplet, String sexe, double poids, double taille, int age) {
+		super(nomComplet, sexe, poids, taille, age);
+	}
 	
 }
