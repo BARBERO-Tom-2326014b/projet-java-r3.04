39267af81f0acddda92200ac4f8a7171282367c6
Foucher Mathias <mathias.foucher@etu.univ-amu.fr>
2024-11-27 17:31:13 +0100

    Merge branch 'main' of git@github.com:BARBERO-Tom-2326014b/projet-java-r3.04.git into main

--- src/personnage/ServiceMedical.java
+++ src/personnage/ServiceMedical.java
@@ -29,7 +29,7 @@ abstract class ServiceMedical {
         return superficie;
     }
 
-    public int getCapaciteMax() {
+    public int getCapaciteMax(creatures) {
         return capaciteMax;
     }
 
@@ -174,6 +174,9 @@ abstract class ServiceMedical {
         // Logique pour augmenter le niveau de la maladie (exemple simple)
         // La classe Maladie devrait avoir une méthode pour évoluer son niveau
         maladie.augmenterNiveau(1);
+        if(maladie.estLetale()) {
+        	creature.estMort();
+        };
     }
 
     // Méthode pour rendre malade une créature (exemple de tomber malade)
