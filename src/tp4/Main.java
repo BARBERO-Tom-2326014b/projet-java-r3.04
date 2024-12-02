8edf3140ab395e0faa8fdf0e11a31d09b122fc7a
Barbero-Tom-2326014b <tom.barbero@etu.univ-amu.fr>
2024-12-02 14:43:09 +0100

    Merge branch 'main' of git@github.com:BARBERO-Tom-2326014b/projet-java-r3.04.git into main

--- src/tp4/Main.java
+++ src/tp4/Main.java
@@ -1,5 +1,6 @@
 package tp4;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,8 +14,8 @@ public class Main {
         // Scanner pour l'entrée utilisateur
         Scanner scanner = new Scanner(System.in);
         boolean quitter = false;
+        ArrayList<Lycanthrope> lycanthropeL = new ArrayList<Lycanthrope>();
         while (!quitter) {
-            // Afficher le menu principal
             System.out.println("\nQue souhaitez-vous faire ?");
             System.out.println("1. Lycanthropes");
             System.out.println("2. Meute");
@@ -30,25 +31,38 @@ public class Main {
                 	System.out.println("\nQue souhaitez-vous faire ?");
                     System.out.println("1. Créer lycanthrope");
                     System.out.println("2. Choisir lycanthrope");
-                    System.out.println("3. Quitter");
+                    System.out.println("3. Afficher lycanthrope");
+                    System.out.println("4. Quitter");
                     int choix2 = scanner.nextInt();
                     scanner.nextLine();
                     
                     switch(choix2) {
                     	case 1:
-                    		System.out.println("\nQuel nom voulez-vous donner ?");
+                    		System.out.println("nQuel nom voulez-vous donner ?");
                     		String Nom = scanner.nextLine();
                             if(random.nextInt(3)==1) {
-                            Lycanthrope lycan = new Lycanthrope(false, random.nextInt(100), random.nextFloat(10),  random.nextInt(10), random.nextInt(10),Nom);
-                            lycan.afficherCaracteristiques();
+	                            Lycanthrope lycan = new Lycanthrope(false, random.nextInt(100), random.nextFloat(10),  random.nextInt(10), random.nextInt(10),Nom);
+	                            lycanthropeL.add(lycan);
+	                            lycan.afficherCaracteristiques();
                             	}
                             else {
                             	Lycanthrope lycan = new Lycanthrope(true, random.nextInt(100), random.nextFloat(10),  random.nextInt(10), random.nextInt(10),Nom);
+                            	lycanthropeL.add(lycan);
                             	lycan.afficherCaracteristiques();
                             	}
                             break;
-                    	
+                    	case 2:
+                    		break;
                     	case 3:
+                    		System.out.println("Liste des lycanthropes : \n");
+                    		System.out.println(lycanthropeL.size());
+                            for (Lycanthrope l : lycanthropeL) {
+                                System.out.println(l.getNom());
+                            }
+                            break;
+                    		
+                    	
+                    	case 4:
                     		break;
                     }
                 	
