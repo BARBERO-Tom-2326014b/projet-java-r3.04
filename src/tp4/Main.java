48025fa677dd0c5d9374f3bc2b287ddd3a1fa766
Barbero-Tom-2326014b <tom.barbero@etu.univ-amu.fr>
2024-12-02 13:57:36 +0100

    main changement

--- src/tp4/Main.java
+++ src/tp4/Main.java
@@ -8,16 +8,16 @@ public class Main {
 	private static Random random = new Random();
     public static void main(String[] args) {
     	
+    	
+    	
         // Scanner pour l'entrée utilisateur
         Scanner scanner = new Scanner(System.in);
         boolean quitter = false;
-
-        System.out.println("Bienvenue dans Fantasy Hospital !");
         while (!quitter) {
             // Afficher le menu principal
             System.out.println("\nQue souhaitez-vous faire ?");
-            System.out.println("1. Créer Lycanthropes");
-            System.out.println("2. Créer meute");
+            System.out.println("1. Lycanthropes");
+            System.out.println("2. Meute");
             System.out.println("3. Créer colonie");
             System.out.println("4. Quitter");
             System.out.print("Votre choix : ");
@@ -27,16 +27,33 @@ public class Main {
 
             switch (choix) {
                 case 1:
-                	System.out.println("Quel nom voulez-vous choisir ?");
-                    String Nom = scanner.nextLine();
-                    if(random.nextInt(3)==1) {
-                    Lycanthrope lycan = new Lycanthrope(false, random.nextInt(100), random.nextFloat(10),  random.nextInt(10), random.nextInt(10),Nom);
-                    lycan.afficherCaracteristiques();
-                    	}
-                    else {
-                    	Lycanthrope lycan = new Lycanthrope(true, random.nextInt(100), random.nextFloat(10),  random.nextInt(10), random.nextInt(10),Nom);
-                    	lycan.afficherCaracteristiques();
+                	System.out.println("\nQue souhaitez-vous faire ?");
+                    System.out.println("1. Créer lycanthrope");
+                    System.out.println("2. Choisir lycanthrope");
+                    System.out.println("3. Quitter");
+                    int choix2 = scanner.nextInt();
+                    scanner.nextLine();
+                    
+                    switch(choix2) {
+                    	case 1:
+                    		System.out.println("\nQuel nom voulez-vous donner ?");
+                    		String Nom = scanner.nextLine();
+                            if(random.nextInt(3)==1) {
+                            Lycanthrope lycan = new Lycanthrope(false, random.nextInt(100), random.nextFloat(10),  random.nextInt(10), random.nextInt(10),Nom);
+                            lycan.afficherCaracteristiques();
+                            	}
+                            else {
+                            	Lycanthrope lycan = new Lycanthrope(true, random.nextInt(100), random.nextFloat(10),  random.nextInt(10), random.nextInt(10),Nom);
+                            	lycan.afficherCaracteristiques();
+                            	}
+                            break;
+                    	
+                    	case 3:
+                    		break;
                     }
+                	
+                    
+                    
                     
                     
                     break;
@@ -54,7 +71,7 @@ public class Main {
 
         scanner.close();
     }
-       
+    
 }
 
 
